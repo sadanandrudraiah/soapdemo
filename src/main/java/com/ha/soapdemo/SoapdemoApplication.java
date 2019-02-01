4296916fbfaf676720f4333e5f12d53d0fa778bb
sadanandrudraiah <sadanand.rud@gmail.com>
2019-02-01 13:28:44 +0530

    initial upload

---
+++ src/main/java/com/ha/soapdemo/SoapdemoApplication.java
@@ -0,0 +1,17 @@
+package com.ha.soapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+
+@SpringBootApplication
+public class SoapdemoApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapdemoApplication.class, args);
+	}
+
+}
+
